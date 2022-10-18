a51c9752a0974cbcbf8c1c560f49b46e2ec1157a
Ottavio8 <paolini.ottavio8@gmail.com>
2022-10-18 11:48:10 +0200

    con l'aiuto della correzione ho terminato la base

--- js/script.js
+++ js/script.js
@@ -4,23 +4,76 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+/** 
+ * SVOLGIMENTO
+ * quando l'utente clicca sul bottone:
+ *  creare 100 square
+ *      creare l'elemento
+*       aggiungere la classe
+        inserire il numero nello span interno
+        aggiungo il click listener dello square
+     ----> al click colorare lo square cliccato di azurro e scrivere in log il numero
+        appendo lo square alla griglia
+        mostrare la griglia
 
-// prendo il bottone dall'html
+.*
+*/
 
-const myBtn = document.getElementById("btn")
-console.log(btn);
-const maxUndred = 100;
-const grid = document.querySelector(".grid");
-console.log(grid);
+document.getElementById("btn").addEventListener("click", startGame);
 
-// creo la funzione del bottone per far partire il gioco
+const grid = document.getElementById("grid");
 
-for(let i = 0; i < maxUndred; i ++){
-    myBtn.addEventListener("click", function(){
-    const newSquare = document.createElement("div");
-    newSquare.classList.add("box");
-    grid.append(newSquare);
-    })    
+
+function startGame () {
+    grid.innerHTML = "";
+    for( let i = 1; i <= 100; i ++){
+        const newSquare = createBox(i);
+        newSquare.addEventListener("click", handleSquareClick);
+        grid.append(newSquare);
+    }
+}
+
+/**
+ * Description funzione che crea un elemento che rappresenta un quadrato della griglia
+ * @param {number} innerNumber - numero da mostrare nel quadrato
+ * @returns {object} elemento html del quadrato
+ */
+function createBox (innerNumber){
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = `<span>${innerNumber}</span>`
+    return box
 }
 
 
+function handleSquareClick() {
+    console.log(parseInt(this.textContent));
+    this.classList.add("clicked");
+  }
+
+
+
+// prendo il bottone dall'html
+
+// const myBtn = document.getElementById("btn")
+// console.log(btn);
+// const maxUndred = 100;
+// const grid = document.querySelector(".grid");
+// console.log(grid);
+
+// // creo la funzione del bottone per far partire il gioco
+
+// for(let i = 0; i < maxUndred; i ++){
+//     myBtn.addEventListener("click", function(){
+//     const newSquare = document.createElement("div");
+//     newSquare.classList.add("box");
+//     grid.append(newSquare);
+//     const cellNumber = i
+//     })    
+// }
+
+
+
+// TEST
+
+// FUNCTION
